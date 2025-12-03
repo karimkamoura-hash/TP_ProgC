d828e502c9fa915eb083d67fc20e55f05f2a7ad2
karimkamoura-hash <karim.kamoura@gmail.com>
2025-12-03 20:19:21 +0100

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,67 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 20;
+    int num2 = 4;
+    char op = '&';   // Changez cette valeur pour tester les opérations
+
+    int resultat;
+
+    switch (op) {
+        case '+':
+            resultat = num1 + num2;
+            printf("%d + %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '-':
+            resultat = num1 - num2;
+            printf("%d - %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '*':
+            resultat = num1 * num2;
+            printf("%d * %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '/':
+            if (num2 != 0) {
+                resultat = num1 / num2;
+                printf("%d / %d = %d\n", num1, num2, resultat);
+            } else {
+                printf("Erreur : division par zero !\n");
+            }
+            break;
+
+        case '%':
+            if (num2 != 0) {
+                resultat = num1 % num2;
+                printf("%d %% %d = %d\n", num1, num2, resultat);
+            } else {
+                printf("Erreur : modulo par zero !\n");
+            }
+            break;
+
+        case '&': // ET binaire
+            resultat = num1 & num2;
+            printf("%d & %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '|': // OU binaire
+            resultat = num1 | num2;
+            printf("%d | %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '~': // NON binaire (unaire, s'applique à num1 seulement)
+            resultat = ~num1;
+            printf("~%d = %d\n", num1, resultat);
+            break;
+
+        default:
+            printf("Operateur '%c' non reconnu !\n", op);
+            break;
+    }
+
+    return 0;
+}
+
 
