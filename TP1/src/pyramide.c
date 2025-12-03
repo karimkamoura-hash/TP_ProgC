3c15236f001e379a978e00c9a42ebe42d581fb1f
karimkamoura-hash <karim.kamoura@gmail.com>
2025-12-03 20:26:29 +0100

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+int main() {
+
+    int n = 5;      // Hauteur de la pyramide
+    int i, j;
+
+    for (i = 1; i <= n; i++) {
+
+        /* 1. Affichage des espaces avant les nombres */
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+
+        /* 2. Affichage des nombres croissants */
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        /* 3. Affichage des nombres décroissants */
+        for (j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        /* Saut de ligne pour passer au niveau suivant */
+        printf("\n");
+    }
+
+    printf("Generation de la pyramide terminee.\n");
+
+    return 0;
+}
