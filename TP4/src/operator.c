320fa5bd3d8b1c063c329ed03ccdceedd3c1bd85
karimkamoura-hash <karim.kamoura@gmail.com>
2025-12-17 19:47:26 +0100

    Update operator.c

--- TP4/src/operator.c
+++ TP4/src/operator.c
@@ -44,71 +44,9 @@ int negation(int num1, int num2)
     (void) num2; // ignoré pour la négation
     return ~num1;
 }
-📄 main.c
-c
-Copier le code
-#include <stdio.h>
-#include <stddef.h>
-#include "operator.h"
 
-int main()
-{
-    int num1, num2;
-    char op;
-    int result = 0;
 
-    printf("Entrez num1 : ");
-    scanf("%d", &num1);
 
-    printf("Entrez num2 : ");
-    scanf("%d", &num2);
-
-    printf("Entrez l'operateur (+, -, *, /, %%, &, |, ~) : ");
-    scanf(" %c", &op); 
-
-    switch(op)
-    {
-        case '+':
-            result = somme(num1, num2);
-            break;
-
-        case '-':
-            result = difference(num1, num2);
-            break;
-
-        case '*':
-            result = produit(num1, num2);
-            break;
-
-        case '/':
-            result = quotient(num1, num2);
-            break;
-
-        case '%':
-            result = modulo(num1, num2);
-            break;
-
-        case '&':
-            result = et_logique(num1, num2);
-            break;
-
-        case '|':
-            result = ou_logique(num1, num2);
-            break;
-
-        case '~':
-            result = negation(num1, num2);
-            break;
-
-        default:
-            printf("Operateur inconnu !\n");
-            return 1;
-    }
-
-    printf("Resultat : %d\n", result);
-
-    return 0;
-}
 
 
 
