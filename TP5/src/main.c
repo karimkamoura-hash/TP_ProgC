8366f7b5dfeb31b496b73d794d3ac7dffcbf436b
karimkamoura-hash <karim.kamoura@gmail.com>
2025-12-17 20:33:08 +0100

    Update main.c

--- TP5/src/main.c
+++ TP5/src/main.c
@@ -1,16 +1,24 @@
-#include "repertoire.h"
+##include "repertoire.h"
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-
-    if (argc != 2) {
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
         printf("Utilisation : %s <nom_du_repertoire>\n", argv[0]);
         return 1;
     }
 
-    char *nom_repertoire = argv[1];
-
-    lire_dossier(nom_repertoire);
+    const char *nom_repertoire = argv[1];
+    lire_dossier_recursif(nom_repertoire);
 
     return 0;
 }
+
+
+
+
+
+
+
+
