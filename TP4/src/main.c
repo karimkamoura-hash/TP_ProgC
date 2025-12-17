236856a5eda0e229d7486292706bf425541e3a0b
karimkamoura-hash <karim.kamoura@gmail.com>
2025-12-17 19:40:03 +0100

    Update main.c

--- TP4/src/main.c
+++ TP4/src/main.c
@@ -1,1 +1,69 @@
+include <stdio.h>
+#include "operator.h"
+
+int main()
+{
+    int num1, num2;
+    char op;
+    int result = 0;
+
+    printf("Entrez num1 : ");
+    scanf("%d", &num1);
+
+    printf("Entrez num2 : ");
+    scanf("%d", &num2);
+
+    printf("Entrez l'operateur (+, -, *, /, %%, &, |, ~) : ");
+    scanf(" %c", &op); 
+
+    switch(op)
+    {
+        case '+':
+            result = somme(num1, num2);
+            break;
+
+        case '-':
+            result = difference(num1, num2);
+            break;
+
+        case '*':
+            result = produit(num1, num2);
+            break;
+
+        case '/':
+            result = quotient(num1, num2);
+            break;
+
+        case '%':
+            result = modulo(num1, num2);
+            break;
+
+        case '&':
+            result = et_logique(num1, num2);
+            break;
+
+        case '|':
+            result = ou_logique(num1, num2);
+            break;
+
+        case '~':
+            result = negation(num1, num2);
+            break;
+
+        default:
+            printf("Operateur inconnu !\n");
+            return 1;
+    }
+
+    printf("Resultat : %d\n", result);
+
+    return 0;
+}
+
+
+
+
+
+
+
 
