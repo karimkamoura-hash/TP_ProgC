9f6d94ed12eda91eacaa190d6dbe6b88e56a5c82
karimkamoura-hash <karim.kamoura@gmail.com>
2025-12-17 20:01:38 +0100

    Create etudiant.h

---
+++ TP4/src/etudiant.h
@@ -0,0 +1,14 @@
+#ifndef ETUDIANT_H
+#define ETUDIANT_H
+
+typedef struct
+{
+    char nom[50];
+    char prenom[50];
+    char adresse[150];
+    int note1;
+    int note2;
+
+} Etudiant;
+
+#endif
